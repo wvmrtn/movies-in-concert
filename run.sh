870110ed198916992ad54e6970e6ab318444347c
wvmrtn <william.vincent.martin@gmail.com>
2020-02-28 16:33:04 +0100

    added log and run file

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# activate environment
+source ~/python_env/chatbot/bin/activate
+
+# run python script
+python ~/Github/movies-in-concert/main.py Interstellar @URE5PNJTS INFO &> ~/Github/movies-in-concert/run_interstellar.log
+
+
